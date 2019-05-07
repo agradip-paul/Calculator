4f0faacb6eab4397fc1573d4d694ae5c1246697f
user <user@192.168.0.2>
2019-05-07 22:10:54 +0530

    Done

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -1,35 +1,35 @@
 package main;
 import java.util.*; 
 
-public class Calculator {
+public class Calculator
+{
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        Scanner in= new Scanner(System.in);
         int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-        int ans;
+        num1 = in.nextInt();
+        num2 = in.nextInt();
+        
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
-        choose = inp.nextInt();
-        switch (choose){
-        case 1:
-            System.out.println(add( num1,num2));
-            break;
-        case 2:
-            System.out.println(sub( num1,num2));
-            break;      
-        case 3:
-            System.out.println(mult( num1,num2));
-            break;
-        case 4:
-            System.out.println(div( num1,num2));
-            break;
-            default:
-                System.out.println("Illigal Operation");
-
-
+        choose = in.nextInt();
+        switch (choose)
+        {
+	        case 1:
+	            System.out.println(add( num1,num2));
+	            break;
+	        case 2:
+	            System.out.println(sub( num1,num2));
+	            break;      
+	        case 3:
+	            System.out.println(mult( num1,num2));
+	            break;
+	        case 4:
+	            System.out.println(div( num1,num2));
+	            break;
+	        default:
+	            System.out.println("Illegal Operation");
         }
 
 
@@ -37,27 +37,22 @@ public class Calculator {
     }
     public static int add(int x, int y)
     {
-    	int result=0;
-//        write your code here 
-    	
+    	int result = x + y;
         return result;
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
-//        write your code here
+    	int result = x - y;
         return result;
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
-//      write your code here        
+    	int result = x * y;        
     	return result;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
-//      write your code here
+    	int result = x / y;
     	return result;
     }
 
